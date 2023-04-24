1881344c9c6abb987f0cbf4a69a8ed6c2a674141
Patircia <422106685@pcpuma.acatlan.mx>
2023-04-24 12:03:59 -0600

    Initial Commit

---
+++ Empleos y Trabqjos/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Empleos y Trabqjos
+//
+//  Created by CEDAM25 on 24/04/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Empleos y Trabqjos/Empleos_y_TrabqjosApp.swift
@@ -0,0 +1,17 @@
+//
+//  Empleos_y_TrabqjosApp.swift
+//  Empleos y Trabqjos
+//
+//  Created by CEDAM25 on 24/04/23.
+//
+
+import SwiftUI
+
+@main
+struct Empleos_y_TrabqjosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
