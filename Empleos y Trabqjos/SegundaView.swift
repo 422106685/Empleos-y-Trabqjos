b127b858f880b13a8b19d233b2c44969bc16631c
Ximena <421114564@pcpuma.acatlan.unam.mx>
2023-04-25 15:50:00 -0600

    ya mero

--- Empleos y Trabqjos/SegundaView.swift
+++ Empleos y Trabqjos/SegundaView.swift
@@ -17,7 +17,11 @@ struct SegundaView: View {
                     .fontWeight(.bold)
                 Image("icono").resizable().frame(width: 400, height: 500)
                 
-                
+                Text("Vivimos en un planeta azul, con océanos y mares que cubren más del 70 % de la superficie de la Tierra. Estos son esenciales para el planeta y el bienestar de la población, regulan el clima, generan la mayor parte del oxígeno que se respira y proporcionan recursos naturales y alimentos. ")
+                  .font(.callout)
+                    .fontWeight(.thin)
+                    .multilineTextAlignment(.center)
+                    .padding(.all, 51.0)
                 
                 
                 NavigationView{
